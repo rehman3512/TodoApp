7b111b7d5f3238ad60d43cc2dfc1ad41a680844e
REHMAN KHAN <rehmankh2256@gmail.com>
2025-11-11 09:34:11 -0800

    Update splashView.dart

--- lib/View/StartView/splashView/splashView.dart
+++ lib/View/StartView/splashView/splashView.dart
@@ -23,7 +23,7 @@ class _SplashViewState extends State<SplashView> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    Future.delayed(Duration(seconds: 3),(){
+    Future.delayed(Duration(seconds: ),(){
     islogin();
     //   isChecked();
       Get.offAndToNamed(AppRoutes.onboardingScreen);
